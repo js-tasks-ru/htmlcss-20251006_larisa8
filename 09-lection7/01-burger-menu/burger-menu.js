8b070dc55b1c5cdfbb239a61d2212ce0e8a7c7ad
Larisa Demidova <laro4ka.f@yandex.ru>
2025-11-13 16:47:20 +0300

    fix: add aria attributes and more accessibility

--- 09-lection7/01-burger-menu/burger-menu.js
+++ 09-lection7/01-burger-menu/burger-menu.js
@@ -6,6 +6,9 @@ if (menuOpen) {
     menuOpen.addEventListener('click', function() {
         if (menu) {
             menu.setAttribute('data-open', '');
+            menu.setAttribute('aria-hidden', false);
+            menuOpen.setAttribute('aria-expanded', true);
+            menuClose.focus();
         }
     });
 }
@@ -14,6 +17,9 @@ if (menuClose) {
     menuClose.addEventListener('click', function() {
         if (menu) {
             menu.removeAttribute('data-open');
+            menu.setAttribute('aria-hidden', true);
+            menuOpen.setAttribute('aria-expanded', false); 
+            menuOpen.focus();
         }
     });
 }
